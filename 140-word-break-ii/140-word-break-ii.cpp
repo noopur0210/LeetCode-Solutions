6b7a969bd26c2e4a41dfea73459d7f4aa5de099a
Noopur Sharma <64369657+noopur0210@users.noreply.github.com>
2022-07-04 20:01:36 +0530

    Time: 9 ms (6.38%), Space: 7.2 MB (82.53%) - LeetHub

--- 140-word-break-ii/140-word-break-ii.cpp
+++ 140-word-break-ii/140-word-break-ii.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
     
-    vector<string> help(string s, vector<string> b){
+    vector<string> help(string s, unordered_set<string>& b){
         if(!s.size()){
             return {""};
         }
         vector<string> ans;
-        set<string> chk;    //check if cur has already occurred
         for(auto cur:b){
-            if(chk.find(cur)!=chk.end()) continue;
-            chk.insert(cur);
             if(s.substr(0,cur.size())==cur){
-                    vector<string> small=help(s.substr(cur.size()),b);
+                vector<string> small=help(s.substr(cur.size()),b);
 
-                    for(auto sen:small){
+                for(auto sen:small){
 
-                        (sen.size())?ans.push_back(cur+" "+sen):ans.push_back(cur);
-                    }
+                    (sen.size())?ans.push_back(cur+" "+sen):ans.push_back(cur);
+                }
             }
         }
         return ans;
     }
     
     vector<string> wordBreak(string s, vector<string>& b) {
-        vector<string> ans= help(s,b);
+        unordered_set<string> set(b.begin(), b.end());
+        vector<string> ans= help(s,set);
         return ans;
     }
 };
